14af50e98944400c57d0bdd0e482d4a4f339f94b
Rafael William <rafael.fim@hotmail.com>
2023-06-08 09:21:02 -0300

    auth implementation

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { KeyboardAvoidingView, Platform, StatusBar } from 'react-native';
 import { 
   Brand, 
@@ -10,8 +11,17 @@ import {
 import brandImg from "@assets/brand.png"
 import { Button } from "@components/Button";
 import { Input } from "@components/Input";
+import { useAuth } from '@hooks/auth';
 
 export function SignIn() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const { signIn, isLogging } = useAuth()
+
+  function handleSignIn() {
+    signIn(email, password)
+  }
+
   return (
     <Container>
       <StatusBar 
@@ -28,17 +38,22 @@ export function SignIn() {
             type="secondary"
             autoCorrect={false}
             autoCapitalize="none"
-          />
+            onChangeText={setEmail}
+            />
           <Input 
             placeholder="Senha"
             type="secondary"
             secureTextEntry
+            onChangeText={setPassword}
           />
           <ForgotPasswordButton>
             <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
           </ForgotPasswordButton>
           <Button 
             title="Entrar"
+            type='secondary'
+            onPress={handleSignIn}
+            isLoading={isLogging}
           />
         </Content>
       </KeyboardAvoidingView>
