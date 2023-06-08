3eac4d987f40f4d345a836a4c9baf8dd90365652
Rafael William <rafael.fim@hotmail.com>
2023-06-08 08:36:26 -0300

    login page finished

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,24 +1,47 @@
+import { KeyboardAvoidingView, Platform, StatusBar } from 'react-native';
+import { 
+  Brand, 
+  Container, 
+  Content, 
+  Title, 
+  ForgotPasswordButton,
+  ForgotPasswordLabel
+} from "./styles";
+import brandImg from "@assets/brand.png"
 import { Button } from "@components/Button";
-import { Container } from "./styles";
 import { Input } from "@components/Input";
 
 export function SignIn() {
   return (
     <Container>
-      <Input 
-        placeholder="E-mail"
-        type="secondary"
-        autoCorrect={false}
-        autoCapitalize="none"
+      <StatusBar 
+        barStyle='light-content'
+        backgroundColor='transparent'
+        translucent
       />
-      <Input 
-        placeholder="Senha"
-        type="secondary"
-        secureTextEntry
-      />
-      <Button 
-        title="Entrar"
-      />
+      <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+        <Content>
+          <Brand source={brandImg}/>
+          <Title>Login</Title>
+          <Input 
+            placeholder="E-mail"
+            type="secondary"
+            autoCorrect={false}
+            autoCapitalize="none"
+          />
+          <Input 
+            placeholder="Senha"
+            type="secondary"
+            secureTextEntry
+          />
+          <ForgotPasswordButton>
+            <ForgotPasswordLabel>Esqueci minha senha</ForgotPasswordLabel>
+          </ForgotPasswordButton>
+          <Button 
+            title="Entrar"
+          />
+        </Content>
+      </KeyboardAvoidingView>
     </Container>
   )
 }
