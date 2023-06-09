3f59fe589d6a4c54b148ab573a7c5daed26b5c83
Rafael William <rafael.fim@hotmail.com>
2023-06-09 14:15:57 -0300

    upload an image in app

--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from 'react';
 import { Platform, TouchableOpacity } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import { 
   Container, 
   Header, 
@@ -11,6 +13,24 @@ import { ButtonBack } from '@components/ButtonBack';
 import { Photo } from '@components/Photo';
 
 export function Product() {
+
+  const [image, setImage] = useState('')
+
+  async function handlePickerImage() {
+    const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync()
+
+    if (status === 'granted') {
+      const result = await ImagePicker.launchImageLibraryAsync({
+        mediaTypes: ImagePicker.MediaTypeOptions.Images,
+        aspect: [4, 4]
+      })
+
+      if (!result.canceled) {
+        setImage(result.assets[0].uri)
+      }
+    }
+  } 
+
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
       <Header>
@@ -22,10 +42,11 @@ export function Product() {
       </Header>
 
       <Upload>
-        <Photo uri=''/>
+        <Photo uri={image} />
         <PickImageButton 
           title='Carregar'
           type='secondary'
+          onPress={handlePickerImage}
         />
       </Upload>
     </Container>  
